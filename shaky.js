12a7bda477816ba26f923645b7b7e2d23f1287e9
Shawn <shawn@neothread.com>
2015-10-01 13:52:55 +0800

    update shaky.json and index.js

--- shaky.js
+++ shaky.js
@@ -1,6 +1,22 @@
 #! /usr/bin/env node
-var print = require('shaky');
-print();
-console.log("console.log output");
-console.log('Arguments:', process.argv.length);  
-console.log(process.argv);
+var Shaky = require('./index.js'),
+    input,
+    output,
+    arg = process.argv;
+    len = arg.length;
+if(arg.length!=6){
+    console.log("Usage: shaky -i ascii.txt -o diagram.png");
+    return 0;
+}
+for (var index = 0; index < len; index++) {
+  switch (arg[index]) {
+   case '-i':
+     input = arg[index+1];
+     break;
+   case '-o':
+     output = arg[index+1];
+     break;
+  }
+}
+
+Shaky(input, output);
